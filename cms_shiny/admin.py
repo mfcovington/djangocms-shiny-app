ac1596d56368dd3a45730d4cf531228157ccd2e5
Mike Covington <mfcovington@gmail.com>
2015-04-15 00:37:02 -0700

    Register ShinyAppAdmin

--- cms_shiny/admin.py
+++ cms_shiny/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from cms_shiny.models import ShinyApp
 
-# Register your models here.
+class ShinyAppAdmin(admin.ModelAdmin):
+
+    fieldset_basic = ('Basic', {
+        'fields': [
+            'name',
+            'description',
+            'url',
+            'repo',
+            'image',
+        ]
+    })
+
+    fieldset_advanced = ('Advanced', {
+        'fields': ['slug'],
+        'classes': ['collapse'],
+    })
+
+    fieldsets = [
+        fieldset_basic,
+        fieldset_advanced,
+    ]
+
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(ShinyApp, ShinyAppAdmin)
