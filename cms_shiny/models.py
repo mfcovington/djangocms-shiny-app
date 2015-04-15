1aa7f82410c38bb079a8ffc41133a0fdddbc6790
Mike Covington <mfcovington@gmail.com>
2015-04-15 00:36:43 -0700

    Create ShinyApp model

--- cms_shiny/models.py
+++ cms_shiny/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from filer.fields.image import FilerImageField
 
-# Create your models here.
+class ShinyApp(models.Model):
+
+    name = models.CharField(
+        help_text='Enter a brief, yet descriptive name for the Shiny app.',
+        max_length=64,
+    )
+
+    description = models.TextField(
+        help_text='Describe the Shiny app in more detail.',
+    )
+
+    url = models.URLField(
+        help_text="Shiny apps are included as iframe elements. Specify the app's URL.",
+    )
+
+    repo = models.URLField(
+        blank=True,
+        help_text="Specify the URL of the Shiny app's repository (e.g., GitHub, Bitbucket, etc.).",
+    )
+
+    image = FilerImageField(
+        blank=True,
+        help_text='Upload a screenshot of the Shiny app.',
+    )
+
+    slug = models.SlugField(u'slug',
+        default='',
+        help_text='Please enter a unique slug for this Shiny app. This should get auto-generated.',
+        max_length=64,
+    )
+
+    def __str__(self):
+        return self.name
