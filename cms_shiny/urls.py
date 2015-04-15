267db7d503a7fcf3c11e137194034ce17b3c28b6
Mike Covington <mfcovington@gmail.com>
2015-04-15 03:15:28 -0700

    Add generic detail and list views

---
+++ cms_shiny/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+from cms_shiny.views import ShinyAppListView, ShinyAppDetailView
+
+urlpatterns = patterns('',
+    url(r'^$', ShinyAppListView.as_view(), name='shiny_list'),
+    url(r'^(?P<slug>[^/]+)$', ShinyAppDetailView.as_view(), name='shiny_detail'),
+)
--- cms_shiny/views.py
+++ cms_shiny/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView
+from cms_shiny.models import ShinyApp
 
-# Create your views here.
+class ShinyAppListView(ListView):
+    model = ShinyApp
+    template_name = 'cms_shiny/shiny_list.html'
+    context_object_name = 'published_shiny_apps'
+
+    def get_queryset(self):
+        """Return all published Shiny apps."""
+        return ShinyApp.objects.filter(publish=True)
+
+
+class ShinyAppDetailView(DetailView):
+    model = ShinyApp
+    template_name = 'cms_shiny/shiny_detail.html'
+    context_object_name = 'shiny_app'
