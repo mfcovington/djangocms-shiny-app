5d44a67c1e416f8024bac1cbef5f3b3516ffd42a
Mike Covington <mfcovington@gmail.com>
2015-08-21 15:51:59 -0700

    Append trailing slash to detail URL

--- cms_shiny/urls.py
+++ cms_shiny/urls.py
@@ -3,5 +3,5 @@ from cms_shiny.views import ShinyAppListView, ShinyAppDetailView
 
 urlpatterns = patterns('',
     url(r'^$', ShinyAppListView.as_view(), name='shiny_list'),
-    url(r'^(?P<slug>[^/]+)$', ShinyAppDetailView.as_view(), name='shiny_detail'),
+    url(r'^(?P<slug>[^/]+)/$', ShinyAppDetailView.as_view(), name='shiny_detail'),
 )
